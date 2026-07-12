ff62a36e1605632cc31261ef29f87d3b012ee2a0
Dolly <sayanhday@gmail.com>
2026-07-12 13:03:41 +0700

    add sound for win/lose popup

--- Assets/_Game/Scripts/UI/ResultPanel.cs
+++ Assets/_Game/Scripts/UI/ResultPanel.cs
@@ -12,6 +12,10 @@ namespace ZombieWar.UI
         [SerializeField] private Text statsText;
         [SerializeField] private GameObject nextLevelButton;
         [SerializeField] private string nextSceneName = "Level2";
+        [SerializeField] private AudioSource stingerSource;
+        [SerializeField] private AudioClip winClip;
+        [SerializeField] private AudioClip loseClip;
+        [SerializeField] private float stingerVolumeBoost = 2.6f; // stand out over gunfire/zombie sfx
 
         private void Awake()
         {
@@ -37,6 +41,12 @@ namespace ZombieWar.UI
                     && Application.CanStreamedLevelBeLoaded(nextSceneName);
                 nextLevelButton.SetActive(hasNext);
             }
+
+            if (stingerSource != null)
+            {
+                var clip = won ? winClip : loseClip;
+                if (clip != null) stingerSource.PlayOneShot(clip, GameSettings.SfxVolume * stingerVolumeBoost);
+            }
         }
 
         public void OnRestartPressed()
@@ -47,6 +57,12 @@ namespace ZombieWar.UI
         public void OnNextLevelPressed()
         {
             if (GameManager.Instance != null) GameManager.Instance.LoadScene(nextSceneName);
+        }
+
+        public void OnHomePressed()
+        {
+            if (SceneLoader.Instance != null) SceneLoader.Load("HomeMenu");
+            else SceneManager.LoadScene("HomeMenu");
         }
     }
 }
